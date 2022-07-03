12f885be85ea5d523304c42fe8352747a656ce67
Kazuho Tawaraya <kazu0711tawa@gmail.com>
2022-07-03 17:55:13 +0900

    edit certification and secret.html

---
+++ php/certification.php
@@ -0,0 +1,21 @@
+<?php
+if (isset($_POST['name'])) {
+  $dsn = 'mysql:dbname=certification;charset=utf8';
+  $user = 'kz';
+  $password = '17830';
+  $dbh = new PDO($dsn, $user, $password);
+
+  $stmt = $dbh->prepare("SELECT * FROM users WHERE name=:user");
+  $stmt->bindParam(':user', $_POST['name']);
+  $stmt->execute();
+  if ($rows = $stmt->fetch()) {
+    if ($rows["password"] ==  $_POST['password']) {
+      /*print "<p>ログイン成功</p>";*/
+      header('Location: http://localhost:80/html/secret.html');
+    } else {
+      print "<p>ログイン失敗(pass)</p>";
+    }
+  } else {
+    print "<p>ログイン失敗(user)</p>";
+  }
+}
