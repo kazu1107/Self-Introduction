0d3f4a4d9035e53de706dcc8e887aa60ded21e03
Kazuho Tawaraya <kazu0711tawa@gmail.com>
2022-08-15 11:48:13 +0900

    add to_do_list.php

---
+++ php/to_do_list.php
@@ -0,0 +1,14 @@
+<?php
+if (isset($_POST['text'])) {
+  $dsn = 'mysql:dbname=to_do_list;charset=utf8';
+  $user = 'kz';
+  $password = '17830';
+  $dbh = new PDO($dsn, $user, $password);
+
+  $text   = $_REQUEST['text'];
+  $sql = "INSERT INTO to_do (text) VALUES (:text)";
+  $stmt = $dbh->prepare($sql);
+  $params = array(':text' => $text);
+  $stmt->execute($params);
+  header('Location: http://localhost:80/html/to_do_list.html');
+}
